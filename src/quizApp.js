14405ecb32c37f95334d688d9c1037a8eed6da8f
thousandjake <thousandjake@gmail.com>
2015-08-16 07:47:07 -0400

    fixed renderQuestion function

--- src/quizApp.js
+++ src/quizApp.js
@@ -1,11 +1,11 @@
 var main = function() {
   function renderQuestion () {
     question = "<h3> "+questions[counter][0]+"</h3>"
-    choiceA = "<input='radio' name='choices' value='A'>"+questions[counter][1]+"<br>";
-    choiceB = "<input='radio' name='choices' value='B'>"+questions[counter][2]+"<br>";
-    choiceC = "<input='radio' name='choices' value='C'>"+questions[counter][3]+"<br>";
-    choiceD = "<input='radio' name='choices' value='D'>"+questions[counter][4]+"<br>";
-    button = "<input='button' name'next' onclick='checkAnswer()'>";
+    choiceA = "<input type='radio' name='choices' value='A'/>"+questions[counter][1]+"<br>";
+    choiceB = "<input type='radio' name='choices' value='B'/>"+questions[counter][2]+"<br>";
+    choiceC = "<input type='radio' name='choices' value='C'/>"+questions[counter][3]+"<br>";
+    choiceD = "<input type='radio' name='choices' value='D'/>"+questions[counter][4]+"<br>";
+    button = "<input type='button' value='next' onclick='checkAnswer()'/>";
 
     $("#quiz").append(question);
     $("#quiz").append(choiceA);
